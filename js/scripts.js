0d1fc2679d12bf387a4d8802721c1d0305ccd12e
BrianHGrant <bhgrant8@gmail.com>
2016-07-07 12:55:44 -0700

    update facebook call

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 // Business logic
 
+var fb_callout = function() {
+  FB.ui({
+    method: 'feed',
+    name: 'Your Perfect Vacation awaits in ' +  $("#" + resultID + " h3").text(),
+    link: 'http://brianhgrant.github.io/perfect-vacation/',
+    description: $("#" + resultID + " p").text(),
+    caption: 'Perfect Vacation Finder',
+    picture: 'http://brianhgrant.github.io/perfect-vacation/' + $("#" + resultID + " img").attr('src'),
+    // display: 'dialog'
+  }, function(response){
+    if (response && response.post_id) {
+      window.location.href = '';
+    } else {
+      alert('Post was not published.');
+    }
+  });
+}
+
 
 var findVacation = function(gender, preference, taste, alcohol, fun, name, age, pleasure) {
   var morals = 0;
@@ -204,7 +222,7 @@ $(document).ready(function(event) {
           picture: 'http://brianhgrant.github.io/perfect-vacation/' + $("#" + resultID + " img").attr('src'),
           display: 'dialog'
         }, function(response){});
-      })
+      });
     }
     event.preventDefault();
   });
